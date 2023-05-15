0e5ccec4333cbe4e17922bf1668d702a838b4986
Jeudy37 <109530582+Jeudy37@users.noreply.github.com>
2023-05-15 01:32:04 -0400

    Update db.config.js

--- db.config.js
+++ db.config.js
@@ -3,7 +3,7 @@ const { Sequelize }= require("sequelize")
 // connection to the DB
 
 let sequelize =new Sequelize(
-    "bibliotheque","jeudy",process.env.DB_PASSWORD,{
+    process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
         host:process.env.DB_HOST,
         port:process.env.DB_PORT,
         dialect:'postgres',
